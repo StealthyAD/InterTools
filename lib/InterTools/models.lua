fffe833355b8ae982fc5af1eb5f3ffd69c657582
Stealthy <125747450+StealthyAD@users.noreply.github.com>
2023-05-17 18:30:26 +0200

    Add files via upload

---
+++ lib/InterTools/models.lua
@@ -0,0 +1,64 @@
+--[[
+
+    ██╗███╗   ██╗████████╗███████╗██████╗ ████████╗ ██████╗  ██████╗ ██╗     ███████╗
+    ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+    ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝   ██║   ██║   ██║██║   ██║██║     ███████╗
+    ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗   ██║   ██║   ██║██║   ██║██║     ╚════██║
+    ██║██║ ╚████║   ██║   ███████╗██║  ██║   ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+    ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+                                                                                    
+    Features:
+    - Compatible All Stand Versions if deprecated versions too.
+    - Complete script.
+
+    Help with Lua?
+    - GTAV Natives: https://nativedb.dotindustries.dev/natives/
+    - FiveM Docs Natives: https://docs.fivem.net/natives/
+    - Stand Lua Documentation: https://stand.gg/help/lua-api-documentation
+    - Lua Documentation: https://www.lua.org/docs.html
+
+    Part: Models
+]]--
+
+    models = {
+        "f15c2",
+        "f16c",
+        "fa18f1",
+        "fa18e",
+        "ac130u",
+        "f22a",
+        "f35c",
+        "f14d2",
+        "f14d1",
+        "f14a2",
+        "f15s",
+        "f15j2",
+        "f15smtd",
+        "a10c",
+        "mq1",
+        "mq9",
+        "f117a",
+        "fb22",
+        "yf22",
+        "darkstar",
+        "a7corsair",
+        "f4",
+        "lazer",
+        "hydra",
+        "strikeforce",
+        "molotok",
+        "rogue",
+        "pyro",
+        "nokota",
+        "buzzard",
+        "savage",
+        "valkyrie",
+        "hunter",
+        "akula",
+        "annihilator",
+        "annihilator2",
+        "cargobob",
+        "starling",
+        "mogul",
+        "seabreeze",
+    }
